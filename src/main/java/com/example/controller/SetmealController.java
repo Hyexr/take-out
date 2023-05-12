71106eac178d370de11487b7944593fbc802df2e
hyx <huangyexuan@outlook.com>
2023-05-12 10:50:14 +0800

    redis缓存

--- src/main/java/com/example/controller/SetmealController.java
+++ src/main/java/com/example/controller/SetmealController.java
@@ -12,6 +12,8 @@ import com.example.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -48,6 +50,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
 
         log.info(setmealDto.toString());
@@ -95,6 +98,7 @@ public class SetmealController {
      * 删除
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
 
         setmealService.removeWithDish(ids);
@@ -106,6 +110,7 @@ public class SetmealController {
      * 根据条件查询套餐
      */
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId+'_'+#setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
 
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
